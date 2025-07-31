1ff1a95e521fc06033e3e036889be02696f27a84
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-07-31 15:07:16 +1000

    Added html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -8,4 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  pligins: [
+    new HtmlWebpackPlugin({
+        template: "./src/template.html",
+    })
+  ]
 };
