a5111359606e4ed8b8313c14a388860c08e52f25
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-07-31 15:09:00 +1000

    Fixed crashes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,9 +10,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  pligins: [
+  plugins: [
     new HtmlWebpackPlugin({
-        template: "./src/template.html",
-    })
-  ]
+      template: "./src/template.html",
+    }),
+  ],
 };
