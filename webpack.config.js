99747f7748939022159f5aa35c3c07ae1a0216eb
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-08-05 08:59:01 +1000

    Changed the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
@@ -25,6 +29,10 @@ module.exports = {
         test: /\.html$/i,
         loader: "html-loader",
       },
-    ]
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
