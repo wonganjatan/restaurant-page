04bf7ea651a780295e018df71b1b4720a26488bb
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-08-05 15:05:35 +1000

    Moved from writing in html to javascript

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import "./styles.css"
-console.log("JS loaded"); // for sanity check
+
+const content = document.querySelector('.content')
+const name = document.createElement('div')
+const introduction = document.createElement('div')
+const hours = document.createElement('div')
+const location = document.createElement('div')
+
+const nameHeader = document.createElement('h1')
+nameHeader.textContent = "Warung Kopi"
+name.appendChild(nameHeader)
+content.appendChild(name)
