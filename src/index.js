d9ae3d74f326a6a89f0b5132e4f5504f7d8c387a
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2025-08-06 13:53:05 +1000

    Moved the name section into smaller function

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ const contact = document.querySelector('#contact')
 const content = document.querySelector('.content')
 
 const homeContent = () => {
-    const name = document.createElement('div')
+    content.appendChild(createNameSection())
+    function createNameSection() {
+        const name = document.createElement('div')
+        const nameHeader = document.createElement('h1')
+        nameHeader.textContent = "Warung Kopi"
+        name.appendChild(nameHeader)
+
+        return name
+    }
     const introduction = document.createElement('div')
     const hours = document.createElement('div')
     const location = document.createElement('div')
 
-    const nameHeader = document.createElement('h1')
-    nameHeader.textContent = "Warung Kopi"
-    name.appendChild(nameHeader)
-    content.appendChild(name)
+    
 
     const introParagraph = document.createElement('p')
     introParagraph.textContent = "Warung Kopi is the best coffee shop in the world"
